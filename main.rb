4e40466b05bffa7a4cda6196b21594804cc0f086
Rafael Donizeti Garcia <PUCMINAS\950112@PPC01INFLL00130.pucminas.net>
2020-02-20 22:15:50 -0300

    #3: metodos

--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 numero_secreto = 121
-index = 0
+index = 1
 
-while index < 3 do
-    puts "Entre com um palpite: "
-    palpite = gets.chomp.to_i
-    puts "Você acertou?"
-    puts palpite == numero_secreto
-    puts" "
-    index = index + 1
+
+def continua(indice)
+    return false if indice >= 3
+    true
 end
+
+def compara(valor, numero)
+    return false if valor == numero
+    true
+end
+
+
+puts "Entre com um palpite: "
+while (compara(palpite = gets.chomp.to_i, numero_secreto)) && (continua(index))
+    puts "Entre com um palpite: "
+    index += 1
+end
+
+
+
+
+
+
+   
